b58905ab2ddbe18d5b45fa2f1579a51c217b58a6
BillyRogers <billy_rogers_07@hotmail.com>
2016-03-11 12:27:40 -0430

    Correccion de rutas

--- upload/include/staff/tickets.inc.php
+++ upload/include/staff/tickets.inc.php
@@ -1,7 +1,7 @@
 <!--Inicio Billy 25/01/2016-->
 
-<link rel="stylesheet" href="/ostickets/upload/css/bootstrap.css">
-  <script src="/ostickets/upload/css/bootstrap.min.js"></script>
+<link rel="stylesheet" href="/upload/css/bootstrap.css">
+  <script src="/upload/css/bootstrap.min.js"></script>
 
 <!--Fin Billy 25/01/2016-->
 
